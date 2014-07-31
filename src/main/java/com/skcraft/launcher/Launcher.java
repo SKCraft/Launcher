befbdfbbf85f383ffde8a72457edee3c10a3a7cb
James137137 <james137137@gmail.com>
2014-07-31 18:07:57 +1200

    undo the changes ( 0.9.4-SNAPSHOT >= 0.9.4)  0.9.4-SNAPSHOT <= 0.9.4

--- src/main/java/com/skcraft/launcher/Launcher.java
+++ src/main/java/com/skcraft/launcher/Launcher.java
@@ -108,14 +108,6 @@ public final class Launcher {
         }
         return version;
     }
-    
-    public String getVersionWithoutSnapshot() {
-        String version = getProperties().getProperty("version");
-        if (version.equals("${project.version}")) {
-            return "1.0.0-SNAPSHOT";
-        }
-        return version.replaceAll("-SNAPSHOT", "");
-    }
 
     /**
      * Get a login service.
