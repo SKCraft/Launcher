75bab533eb5c57514d4970d26263b9473a5d753b
Penagwin <paul@penagw.in>
2014-12-02 11:23:42 -0500

    Turns out imports are 'import'ant!

--- src/main/java/com/skcraft/launcher/swing/WebpagePanel.java
+++ src/main/java/com/skcraft/launcher/swing/WebpagePanel.java
@@ -20,9 +20,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Enumeration;
 import java.util.logging.Level;
 
