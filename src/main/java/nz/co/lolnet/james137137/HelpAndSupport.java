fba4ce5c65b08af94599eddd3d539801e5492af0
James Anderson <james137137@gmail.com>
2015-02-09 14:01:43 +1300

    formatted HelpAndSupport.java to make it easier

--- src/main/java/nz/co/lolnet/james137137/HelpAndSupport.java
+++ src/main/java/nz/co/lolnet/james137137/HelpAndSupport.java
@@ -1,11 +1,10 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package nz.co.lolnet.james137137;
 
-import com.skcraft.launcher.dialog.LauncherFrame;
 import java.awt.Desktop;
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -21,50 +20,6 @@ import javax.swing.JOptionPane;
  */
 public class HelpAndSupport {
 
-    public static void Start() {
-        //Custom button text
-        Object[] options = {"LolnetLauncher", "My account"};
-        int answer = JOptionPane.showOptionDialog(null,
-                "What can I help you with?",
-                "LolnetLauncher Help",
-                JOptionPane.YES_NO_CANCEL_OPTION,
-                JOptionPane.QUESTION_MESSAGE,
-                null,
-                options,
-                null);
-        if (answer == 0) {
-            Object[] options1_1 = {"I found an error/bug", "I have a suggestion for the Launcher", "I would like to submit a modpack"};
-            answer = JOptionPane.showOptionDialog(null,
-                    "What subject on LolnetLauncher",
-                    "LolnetLauncher Help",
-                    JOptionPane.YES_NO_CANCEL_OPTION,
-                    JOptionPane.QUESTION_MESSAGE,
-                    null,
-                    options1_1,
-                    null);
-            if (answer == 0) {
-                sendToLauncherBugHelp();
-            } else if (answer == 1) {
-                sendToLauncherRequest();
-            } else if (answer == 2) {
-                goToNewModPackSummition();
-            }
-        } else if (answer == 1) {
-            Object[] options1_1 = {"I can't login", "I forgot my password", "I don't have a username"};
-            answer = JOptionPane.showOptionDialog(null,
-                    "What is the problem.",
-                    "Account Help",
-                    JOptionPane.YES_NO_CANCEL_OPTION,
-                    JOptionPane.QUESTION_MESSAGE,
-                    null,
-                    options1_1,
-                    null);
-            if (answer == 0) {
-                CantLogin();
-            }
-        }
-    }
-
     public static boolean openURL(String toURL) {
 
         try {
@@ -77,15 +32,127 @@ public class HelpAndSupport {
         }
     }
 
+    public static void Start() {
+        //Custom button text
+        Object[] options = {"LolnetLauncher", "My account"};
+        int answer = JOptionPane.showOptionDialog(null,
+                "What can I help you with?",
+                "LolnetLauncher Help",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                null);
+        if (answer == 0) {
+            LauncherHelp();
+        } else if (answer == 1) {
+            AccountHelp();
+        }
+    }
+
+    private static void LauncherHelp() {
+        Object[] options = {"I found an error/bug", "I have a suggestion for the Launcher", "I would like to submit a modpack"};
+        int answer = JOptionPane.showOptionDialog(null,
+                "What subject on LolnetLauncher",
+                "LolnetLauncher Help",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                null);
+        if (answer == 0) {
+            sendToLauncherBugHelp();
+        } else if (answer == 1) {
+            sendToLauncherRequest();
+        } else if (answer == 2) {
+            goToNewModPackSummition();
+        }
+    }
+    
+    private static void sendToLauncherBugHelp() {
+        Object[] options = {"Take me there", "Make a post now", "Cancel"};
+        int answer = JOptionPane.showOptionDialog(null,
+                "It is suggested that you read the FAQ before posting.",
+                "LolnetLauncher Help",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                null);
+        if (answer == 0) {
+            openURL("https://lolnet.co.nz/viewtopic.php?f=135&t=5684");
+        } else if (answer == 1) {
+            openURL("https://lolnet.co.nz/posting.php?mode=post&f=135");
+        }
+    }
+
+    private static void sendToLauncherRequest() {
+        Object[] options = {"Take me there", "Cancel"};
+        int answer = JOptionPane.showOptionDialog(null,
+                "We are always greatful for suggestion and feedback.\nYou can do so by making a post on the forum",
+                "LolnetLauncher Help",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                null);
+        if (answer == 0) {
+            openURL("https://lolnet.co.nz/viewforum.php?f=135");
+        }
+    }
+
+    private static void AccountHelp() {
+        Object[] options = {"I can't login", "I forgot my password", "I don't have a username"};
+        int answer = JOptionPane.showOptionDialog(null,
+                "What is the problem.",
+                "Account Help",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                null);
+        if (answer == 0) {
+            CantLogin();
+        }
+    }
+    
+    private static void goToNewModPackSummition() {
+        try {
+            URL url = new URL("https://www.lolnet.co.nz/modpack/modPackSummition.html");
+            HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+            int responseCode = huc.getResponseCode();
+            if (responseCode != 404) {
+                Object[] options = {"Take me there", "Cancel"};
+                int answer = JOptionPane.showOptionDialog(null,
+                        "If you have forgeten your password you might need to reset it.",
+                        "Launcher Help",
+                        JOptionPane.YES_NO_CANCEL_OPTION,
+                        JOptionPane.QUESTION_MESSAGE,
+                        null,
+                        options,
+                        null);
+                if (answer == 0) {
+                    openURL(url.toString());
+                }
+
+            } else {
+                JOptionPane.showMessageDialog(null, "Feature comming soon");
+            }
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "Unable to find infomation on that...", "Lolnet ModPack", JOptionPane.INFORMATION_MESSAGE);
+        }
+
+    }
+
     private static void CantLogin() {
-        Object[] options1_2 = {"In game", "Launcher"};
+        Object[] options = {"In game", "Launcher"};
         int answer = JOptionPane.showOptionDialog(null,
                 "Is this ingame or on the launcher?",
                 "Account Help",
                 JOptionPane.YES_NO_CANCEL_OPTION,
                 JOptionPane.QUESTION_MESSAGE,
                 null,
-                options1_2,
+                options,
                 null);
         if (answer == 0) {
             CantLogin_InGame();
@@ -95,21 +162,20 @@ public class HelpAndSupport {
     }
 
     private static void CantLogin_InGame() {
-        Object[] options1_2 = {"Yes", "No", "I don't know..."};
+        Object[] options = {"Yes", "No", "I don't know..."};
         int answer = JOptionPane.showOptionDialog(null,
                 "Are you banned?",
                 "Account Help",
                 JOptionPane.YES_NO_CANCEL_OPTION,
                 JOptionPane.QUESTION_MESSAGE,
                 null,
-                options1_2,
+                options,
                 null);
         if (answer == 0) {
             openURL("https://lolnet.co.nz/viewforum.php?f=34");
         } else if (answer == 1) {
             sendToMinecraftSupportPage();
-        } else if (answer == 2)
-        {
+        } else if (answer == 2) {
             sendToMinecraftSupportPage();
         }
     }
@@ -127,19 +193,19 @@ public class HelpAndSupport {
         if (answer == 0) {
             sendToChangeMojangPassword();
         } else if (answer == 1) {
-            JOptionPane.showMessageDialog(null, "Just add your ingame username and press \"Play offline\" on the bottom left","Launcher offline mode feature",JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Just add your ingame username and press \"Play offline\" on the bottom left", "Launcher offline mode feature", JOptionPane.INFORMATION_MESSAGE);
         }
     }
 
     private static void sendToMinecraftSupportPage() {
-        Object[] options1_2 = {"Take me there", "Cancel"};
+        Object[] options = {"Take me there", "Cancel"};
         int answer = JOptionPane.showOptionDialog(null,
                 "It seems that it would be best to make a post on our forums",
                 "Account Help",
                 JOptionPane.YES_NO_CANCEL_OPTION,
                 JOptionPane.QUESTION_MESSAGE,
                 null,
-                options1_2,
+                options,
                 null);
         if (answer == 0) {
             openURL("https://lolnet.co.nz/posting.php?mode=post&f=134");
@@ -147,79 +213,17 @@ public class HelpAndSupport {
     }
 
     private static void sendToChangeMojangPassword() {
-        Object[] options1_2 = {"Take me there", "Cancel"};
+        Object[] options = {"Take me there", "Cancel"};
         int answer = JOptionPane.showOptionDialog(null,
                 "If you have forgeten your password you might need to reset it.",
                 "Account Help",
                 JOptionPane.YES_NO_CANCEL_OPTION,
                 JOptionPane.QUESTION_MESSAGE,
                 null,
-                options1_2,
+                options,
                 null);
         if (answer == 0) {
             openURL("https://minecraft.net/resetpassword");
-        }
-    }
-
-    private static void goToNewModPackSummition() {
-        try {
-            URL url = new URL("https://www.lolnet.co.nz/modpack/modPackSummition.html");
-            HttpURLConnection huc = (HttpURLConnection) url.openConnection();
-            int responseCode = huc.getResponseCode();
-            if (responseCode != 404) {
-                Object[] options1_2 = {"Take me there", "Cancel"};
-                int answer = JOptionPane.showOptionDialog(null,
-                        "If you have forgeten your password you might need to reset it.",
-                        "Account Help",
-                        JOptionPane.YES_NO_CANCEL_OPTION,
-                        JOptionPane.QUESTION_MESSAGE,
-                        null,
-                        options1_2,
-                        null);
-                if (answer == 0) {
-                    openURL(url.toString());
-                }
-                
-            } else
-            {
-                JOptionPane.showMessageDialog(null, "Feature comming soon");
-            }
-        } catch (IOException ex) {
-            JOptionPane.showMessageDialog(null, "Unable to find infomation on that...","Lolnet ModPack",JOptionPane.INFORMATION_MESSAGE);
-        }
-
-    }
-
-    private static void sendToLauncherBugHelp() {
-        Object[] options1_2 = {"Take me there","Make a post now", "Cancel"};
-        int answer = JOptionPane.showOptionDialog(null,
-                "It is suggested that you read the FAQ before posting.",
-                "LolnetLauncher Help",
-                JOptionPane.YES_NO_CANCEL_OPTION,
-                JOptionPane.QUESTION_MESSAGE,
-                null,
-                options1_2,
-                null);
-        if (answer == 0) {
-            openURL("https://lolnet.co.nz/viewtopic.php?f=135&t=5684");
-        } else if (answer == 1)
-        {
-            openURL("https://lolnet.co.nz/posting.php?mode=post&f=135");
-        }
-    }
-
-    private static void sendToLauncherRequest() {
-        Object[] options1_2 = {"Take me there", "Cancel"};
-        int answer = JOptionPane.showOptionDialog(null,
-                "We are always greatful for suggestion and feedback.\nYou can do so by making a post on the forum",
-                "LolnetLauncher Help",
-                JOptionPane.YES_NO_CANCEL_OPTION,
-                JOptionPane.QUESTION_MESSAGE,
-                null,
-                options1_2,
-                null);
-        if (answer == 0) {
-            openURL("https://lolnet.co.nz/viewforum.php?f=135");
         }
     }
 }
