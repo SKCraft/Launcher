e3f31485168e8089962e6399e4f57eb827851953
James Anderson <james137137@gmail.com>
2015-02-18 10:43:08 +1300

    removed API (not needed for the bootstrap)

--- runme.bat
+++
@@ -1,7 +0,0 @@
-pause
-
-mvn deploy:deploy-file -Durl=file:///D:/Users/James/Documents/GitHub/LolnetLauncher/repo/ -DrepositoryId=file.repo -Dfile=D:/Users/James/Documents/GitHub/LolnetLauncher/LolnetLauncherAPI.jar -DgroupId=nz.co.lolnet -DartifactId=LolnetLauncherAPI -Dversion=0.0.1 -Dpackaging=jar
-
-
-
-pause
