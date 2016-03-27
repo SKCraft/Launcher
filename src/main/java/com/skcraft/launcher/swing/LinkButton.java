48de39358631616c6eb04186af7e00be4d8f3238
James Anderson <james137137@gmail.com>
2016-03-27 13:55:53 +1300

    changed link colour for forgot password

--- src/main/java/com/skcraft/launcher/swing/LinkButton.java
+++ src/main/java/com/skcraft/launcher/swing/LinkButton.java
@@ -14,7 +14,7 @@ import java.awt.event.MouseEvent;
 
 public class LinkButton extends JButton {
 
-    private static final Color LINK_COLOR = Color.blue;
+    private static final Color LINK_COLOR = Color.GRAY;
     private static final Border LINK_BORDER = BorderFactory.createEmptyBorder(0, 0, 1, 0); 
     private static final Border HOVER_BORDER = BorderFactory.createMatteBorder(0, 0, 1, 0, LINK_COLOR);
     
