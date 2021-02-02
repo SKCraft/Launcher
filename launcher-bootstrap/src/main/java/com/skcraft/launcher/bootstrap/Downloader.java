662162392e83694f34f49d7f85593ea382014167
Henry Le Grys <henry@legrys.info>
2021-02-02 03:18:22 +0000

    Allow bootstrap server to announce unpacked update jars
    Pack200 is dead, this is the start of its removal

--- launcher-bootstrap/src/main/java/com/skcraft/launcher/bootstrap/Downloader.java
+++ launcher-bootstrap/src/main/java/com/skcraft/launcher/bootstrap/Downloader.java
@@ -61,11 +61,10 @@ public class Downloader implements Runnable, ProgressObservable {
             }
         });
 
-        File finalFile = new File(bootstrap.getBinariesDir(), System.currentTimeMillis() + ".jar.pack");
-        File tempFile = new File(finalFile.getParentFile(), finalFile.getName() + ".tmp");
         URL updateUrl = HttpRequest.url(bootstrap.getProperties().getProperty("latestUrl"));
 
         log.info("Reading update URL " + updateUrl + "...");
+        List<LauncherBinary> binaries = new ArrayList<LauncherBinary>();
 
         try {
             String data = HttpRequest
@@ -93,6 +92,16 @@ public class Downloader implements Runnable, ProgressObservable {
 
             checkInterrupted();
 
+            boolean packed = true;
+            Object packedCheck = ((JSONObject) object).get("packed");
+            if (packedCheck == Boolean.FALSE) {
+                packed = false;
+            }
+
+            String extension = packed ? ".jar.pack" : ".jar";
+            File finalFile = new File(bootstrap.getBinariesDir(), System.currentTimeMillis() + extension);
+            File tempFile = new File(finalFile.getParentFile(), finalFile.getName() + ".tmp");
+
             log.info("Downloading " + url + " to " + tempFile.getAbsolutePath());
 
             httpRequest = HttpRequest.get(url);
@@ -103,6 +112,9 @@ public class Downloader implements Runnable, ProgressObservable {
 
             finalFile.delete();
             tempFile.renameTo(finalFile);
+
+            LauncherBinary binary = new LauncherBinary(finalFile);
+            binaries.add(binary);
         } finally {
             SwingUtilities.invokeLater(new Runnable() {
                 @Override
@@ -113,9 +125,6 @@ public class Downloader implements Runnable, ProgressObservable {
             });
         }
 
-        LauncherBinary binary = new LauncherBinary(finalFile);
-        List<LauncherBinary> binaries = new ArrayList<LauncherBinary>();
-        binaries.add(binary);
         bootstrap.launchExisting(binaries, false);
     }
 
